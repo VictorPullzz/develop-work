d40bbdc73824633bc1438f35dd5adb88d2a302cf
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-09-19 02:24:15 +0400

    Final changes

--- dev/js/bananas.js
+++ dev/js/bananas.js
@@ -7,6 +7,39 @@
 		$sel.body = $("body", $sel.html);
 
 		return {
+			common: {
+				go: function(topPos, speed, callback) {
+					var curTopPos = $sel.window.scrollTop(),
+						diffTopPos = Math.abs(topPos - curTopPos);
+					$sel.body.add($sel.html).animate({
+						"scrollTop": topPos
+					}, speed, function() {
+						if(callback) {
+							callback();
+						}
+					});
+				}
+			},
+
+			goEl: function() {
+				var self = this;
+					$goEl = $("[data-goto]");
+
+				$goEl.on("click", function(event) {
+					var $nameEl = $sel.body.find($(this).data("goto"));
+
+					if ($nameEl.length === 0) {
+						alert("Возможно вы не правильно указали элемент");
+						return;
+					} else {
+						var posEl = $nameEl.offset().top;
+						BANANAS.common.go(posEl-90, 1000);
+					}
+					event.preventDefault();
+				});
+
+			},
+
 			slider: function() {
 				var self = this,
 					$slick = $(".intro-slider"),
@@ -32,10 +65,11 @@
 				init: function() {
 					var self = this;
 
-					self.$services = $("#services");
+					self.$services = $(".services--home");
 					self.$servicesItems = self.$services.find(".services-item");
 
 					self.createSimpleStructure();
+					self.createFullStructure.init();
 				},
 
 				// method for create grid services //
@@ -90,8 +124,126 @@
 
 						})($(this));
 					});
-				}
+				},
 
+				// ajaxLoader: function() {
+				// 	$sel.body.on("click", ".load-more", function(event) {
+				// 		var $linkAddress = $(this),
+				// 			href = $linkAddress.attr("href"),
+				// 			selector = $linkAddress.data("itemsselector"),
+				// 			$container = $($linkAddress.data("container"));
+				//
+				// 		$linkAddress.addClass("loading");
+				//
+				// 		(function(href, $container, $link, selector) {
+				// 			$.ajax({
+				// 				url: href,
+				// 				success: function(data) {
+				// 					var $data = $('<div />').append(data),
+				// 						$items = $data.find(selector),
+				// 						$preloader = $data.find(".load");
+				//
+				// 					$items.addClass("load-events-item");
+				// 					$container.append($items);
+				// 					$link.parent().remove();
+				//
+				// 					if($preloader && $preloader.length) {
+				// 						$container.parent().append($preloader);
+				// 					}
+				//
+				// 					setTimeout(function() {
+				// 						var timer = 50,
+				// 							$getElements = $container.find(".load-events-item");
+				//
+				// 						$getElements.each(function() {
+				// 							var el = $(this);
+				// 							setTimeout(function() {
+				// 								el.removeClass("load-events-item");
+				// 							}, timer);
+				// 							timer += 100;
+				// 						});
+				//
+				// 						$linkAddress.removeClass("loading");
+				// 					}, 100);
+				//
+				// 					setTimeout(function() {
+				// 						FOODGEOGRAPHY.reload();
+				// 					},200)
+				// 				}
+				// 			})
+				// 		})(href, $container, $linkAddress, selector);
+				// 		event.preventDefault();
+				// 	})
+				// },
+
+				createFullStructure: {
+
+					init: function() {
+						var self = this;
+
+						self.eventButton();
+						self.create();
+					},
+
+					eventButton: function() {
+						var self = this,
+							$showServices = $("[data-services-more]");
+
+						$showServices.each(function() {
+							var $el = $(this),
+								link = $el.attr("href"),
+								elAjaxContent = $el.data("container");
+
+							if (elAjaxContent) {
+								parseAjax = function(mfpResponse) {
+									mfpResponse.data = $(mfpResponse.data).find(elAjaxContent);
+								}
+							} else {
+								parseAjax = null;
+							}
+
+							$el.magnificPopup({
+								items: {
+									src: link,
+									type: "ajax",
+								},
+								tLoading: "Загрузка...",
+								closeMarkup: '<button type="button" class="mfp-close btn-container-close"><svg data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.63 11.63"><defs/><path d="M.31.31l11 11m-11 0l11-11"/></svg></button>',
+								mainClass: "mfp-services",
+								removalDelay: 300,
+								closeOnBgClick: false,
+								closeBtnInside: true,
+								callbacks: {
+									open: function($el) {
+										$(".btn-container-close").on("click", function() {
+											$.magnificPopup.close();
+										});
+									},
+									ajaxContentAdded: function() {
+										//BANANAS.reload();
+										$(".btn-container-close").on("click", function() {
+											$.magnificPopup.close();
+										});
+										self.create();
+									},
+									parseAjax: parseAjax,
+								}
+							});
+						})
+
+					},
+
+					create: function() {
+						var self = this,
+							$gridFullConatiner = $(".services--full .services-clearfix");
+
+						if ($gridFullConatiner.length > 0) {
+							startX = $gridFullConatiner.offset().left,
+							finishX = $gridFullConatiner.offset().left + $gridFullConatiner.width();
+
+						}
+					}
+				}
 			},
 
 			miniScripts: {
@@ -100,13 +252,14 @@
 					var self = this;
 
 					self.fixedLogo();
+					self.preloader.init();
 				},
 
 				fixedLogo: function() {
 					$sel.window.on("scroll", function() {
 						var hh = $(".page-header").outerHeight(),
 							sTop = $sel.window.scrollTop();
-						console.log(sTop);
+
 						if(sTop > hh + 50) {
 							$sel.body.addClass("fixed-logo");
 						} else {
@@ -115,6 +268,38 @@
 					});
 
 				},
+				preloader: {
+
+					preloaderContainer : null,
+
+					init: function() {
+						var self= this;
+
+						self.preloaderContainer = $(".preloader");
+
+						self.showHidePreloader();
+					},
+
+
+					showHidePreloader: function() {
+						var self = this;
+
+						self.preloaderContainer.addClass("active");
+
+						$sel.window.on("load", function() {
+
+							setTimeout(function() {
+								self.preloaderContainer.addClass("hide");
+							}, 1500);
+							setTimeout(function() {
+								self.preloaderContainer.remove();
+							}, 3500);
+						});
+
+					}
+
+				},
+
 
 			}
 
@@ -122,8 +307,9 @@
 
 	})();
 
-	BANANAS.slider()
-	BANANAS.gridServices.init()
-	BANANAS.miniScripts.init()
+	BANANAS.slider();
+	BANANAS.goEl();
+	BANANAS.gridServices.init();
+	BANANAS.miniScripts.init();
 
 })(jQuery);
